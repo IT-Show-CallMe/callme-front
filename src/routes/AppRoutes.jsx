fc1b36a0f6f9de5508722a48fc60cbbf8ab91286
김가현 <rkgus0324@gmail.com>
2025-04-13 19:24:37 +0900

    Fix: 파일 경로 에러 수정

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from '../Pages/HomePage';
-import LandingPage from '../pages/Landing/LandingPage';
-import NicknamePage from '../pages/Nickname/NicknamePage';
-import MainPage from '../pages/Main/MainPage';
-import IdolDetailPage from '../pages/IdolDetail/IdolDetailPage';
+import LandingPage from '../Pages/Landing';
+import NicknamePage from '../pages/Nickname';
+// import MainPage from '../pages/Main/Home';
+// import IdolDetailPage from '../pages/';
 
-import ConfirmNicknamePage from '../pages/VideoCall/ConfirmNicknamePage';
-import EmailAgreementPage from '../pages/VideoCall/EmailAgreementPage';
-import EmailInputPage from '../pages/VideoCall/EmailInputPage';
-import VideoCallPage from '../pages/VideoCall/VideoCallPage';
+// import ConfirmNicknamePage from '../pages/VideoCall/ConfirmNicknamePage';
+// import EmailAgreementPage from '../pages/';
+// import EmailInputPage from '../pages/VideoCall/EmailInputPage';
+// import VideoCallPage from '../pages/VideoCall/VideoCallPage';
 
 import LetterPage from '../pages/Letter/LetterPage';
 
@@ -20,13 +20,13 @@ const AppRoutes = () => {
                 <Route path="/" element={<HomePage />}/>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/nickname" element={<NicknamePage />} />
-                <Route path="/main" element={<MainPage />} />
-                <Route path="/idol/:idolId" element={<IdolDetailPage />} />
+                {/* <Route path="/main" element={<MainPage />} /> */}
+                {/* <Route path="/idol/:idolId" element={<IdolDetailPage />} /> */}
 
-                <Route path="/call/confirm" element={<ConfirmNicknamePage />} />
-                <Route path="/call/agreement" element={<EmailAgreementPage />} />
-                <Route path="/call/email" element={<EmailInputPage />} />
-                <Route path="/call/start" element={<VideoCallPage />} />
+                {/* <Route path="/call/confirm" element={<ConfirmNicknamePage />} /> */}
+                {/* <Route path="/call/agreement" element={<EmailAgreementPage />} /> */}
+                {/* <Route path="/call/email" element={<EmailInputPage />} /> */}
+                {/* <Route path="/call/start" element={<VideoCallPage />} /> */}
 
                 <Route path="/letter" element={<LetterPage />} />
             </Routes>
