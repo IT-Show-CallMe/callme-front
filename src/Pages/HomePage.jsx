894119daedf82b4f5a2376b487a9d21eda3865c7
33ueowon <w2308@e-mirim.hs.kr>
2025-04-29 19:51:56 +0900

    Feat. HomePage 수정, 배경 이미지 추가

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,24 +1,37 @@
 import React from "react";
 import "../styles/homePage.css";
 
-const imagesFiles=[
-    "images/mark_bg.png","images/mjh_bg.png","images/sw_bg.png","images/sh_bg.png","images/so_bg.png","images/yj_bg.png","images/wy_bg.png",
-    "images/ys_bg.png","images/yuj_bg.png","images/lh_bg.png","images/jh_bg.png","images/krn_bg.png","images/hn_bg.png"
-];  
-// 메인 대시보드 역할, 사용자에게 주요 콘텐츠를 보여주는 곳
-function HomePage(){
-    
-    return(
-        <div className="image-grid">
-        {imagesFiles.map((File, i) => (
+// 이미지 리스트
+const imagesFiles = [
+  "images/mark_bg.png", "images/mjh_bg.png", "images/sw_bg.png",
+  "images/sh_bg.png", "images/so_bg.png", "images/yj_bg.png",
+  "images/wy_bg.png", "images/ys_bg.png", "images/yuj_bg.png",
+  "images/lh_bg.png", "images/jh_bg.png", "images/krn_bg.png",
+  "images/hn_bg.png", "images/mj_bg.png"
+];
+
+function HomePage() {
+  return (
+    <div className="Home-container">
+        <img src="images/back_short.png" alt="배경이미지" className="background-img" />
+        <img src="images/home_imgback.png" alt="home background" className="idol-back"/>
+
+
+      <div className="image-grid">
+        {imagesFiles.map((file, i) => {
+          const name = file.split("/")[1].split("_")[0]; // ex: mark
+          return (
             <img 
-            key={i}
-            src={File}
-            alt={`idol-${i}`}
+              key={i}
+              src={file}
+              alt={name}
+              className={`idol-img ${name}`} // ex: class="idol-img mark"
             />
-        ))}
-        </div>
-        
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
 export default HomePage;
