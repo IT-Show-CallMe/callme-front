4aa483b713c3ae76a4480ed2dd95dea7bedd3872
김가현 <rkgus0324@gmail.com>
2025-04-29 23:30:45 +0900

    ✨ Feat: 경로 수정 ( Main, Search, Letter, Review )

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../Pages/HomePage';
 import LandingPage from '../Pages/Landing';
-import NicknamePage from '../pages/Nickname';
-// import MainPage from '../pages/Main/Home';
+import NicknamePage from '../Pages/Nickname';
+import MainPage from '../Pages/Main';
+import SearchPage from '../Pages/Search';
+import LetterPage from '../Pages/Letter';
+import ReviewPage from '../Pages/Review';
+
 // import IdolDetailPage from '../pages/';
 // import ConfirmNicknamePage from '../pages/VideoCall/ConfirmNicknamePage';
 // import EmailAgreementPage from '../pages/';
 // import EmailInputPage from '../pages/VideoCall/EmailInputPage';
 // import VideoCallPage from '../pages/VideoCall/VideoCallPage';
-// import LetterPage from '../pages/Letter/LetterPage';
 
 const AppRoutes = () => {
     return (
@@ -17,13 +20,15 @@ const AppRoutes = () => {
             <Route path="/" element={<HomePage />} />
             <Route path="/landing" element={<LandingPage />} />
             <Route path="/nickname" element={<NicknamePage />} />
-            {/* <Route path="/main" element={<MainPage />} /> */}
+            <Route path="/main" element={<MainPage />} />
+            <Route path="/search" element={<SearchPage />} />
+            <Route path="/letter" element={<LetterPage />} />
+            <Route path="/review" element={<ReviewPage />} />
             {/* <Route path="/idol/:idolId" element={<IdolDetailPage />} /> */}
             {/* <Route path="/call/confirm" element={<ConfirmNicknamePage />} /> */}
             {/* <Route path="/call/agreement" element={<EmailAgreementPage />} /> */}
             {/* <Route path="/call/email" element={<EmailInputPage />} /> */}
             {/* <Route path="/call/start" element={<VideoCallPage />} /> */}
-            {/* <Route path="/letter" element={<LetterPage />} /> */}
         </Routes>
     );
 };
