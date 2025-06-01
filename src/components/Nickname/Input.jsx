21a62e9098e38d226a7b7f79184b67c68bba998e
33ueowon <w2308@e-mirim.hs.kr>
2025-06-01 17:10:03 +0900

    Feat. EmailInput Page <DB>

--- src/components/Nickname/Input.jsx
+++ src/components/Nickname/Input.jsx
@@ -19,26 +19,9 @@ const NicknameInput = () => {
       });
       alert(response.data);
     } catch (error) {
-      console.error("닉네임 저장 실패: ", error);
+      console.error("이메일 저장 실패: ", error);
       alert("닉네임 저장 중 오류가 발생하였습니다. 다시 시도하세요.");
     }
   };
 
-  return (
-    <div className="window-content">
-      <p className="input-title">닉네임을 입력하세요.</p>
-      <input
-        type="text"
-        placeholder="닉네임을 입력하세요."
-        className="input-nickname"
-        value={nickname}
-        onChange={handleInputChange}
-      />
-      <button className="confirm-button" onClick={handleSubmit}>
-        확인
-      </button>
-    </div>
-  );
-};
-
-export default NicknameInput;
+}
