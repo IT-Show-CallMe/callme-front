631a7e6ea170bffae38f91e19efdf7c4993775d9
김가현 <rkgus0324@gmail.com>
2025-06-01 11:42:00 +0900

    feat : IdolModal 컴포넌트 분리 (+IdolCard) 및 MainPage(Top5Idols) 조회수 집계 기능 구현 + CallIncomingPage 사용

--- src/components/IdolModal.jsx
+++ src/components/IdolModal.jsx
@@ -1,17 +1,27 @@
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import styles from "../styles/IdolModal.module.css";
+import IdolCard from "./IdolCard";
 import { useNavigate } from "react-router-dom";
-
 
 export default function IdolModal({ imgUrl, group, name, count, onClose }) {
     const navigate = useNavigate();
 
+    const handleCall = () => {
+        navigate(`/call/incoming/${name}`, { state: { name } });
+    };
     return (
         <div className={styles.modalContainer} onClick={onClose}>
             <div className={styles.contentBox} onClick={(e) => e.stopPropagation()}>
                 {/* <button className={styles.closeBtn} onClick={onClose}>×</button> */}
-                <img className={styles.idolImg} src={imgUrl} alt={name} />
-                <div className={styles.card}>
+                {/* <img className={styles.idolImg} src={imgUrl} alt={name} /> */}
+                <IdolCard
+                    imgUrl={imgUrl}
+                    group={group}
+                    name={name}
+                    count={count}
+                    onCall={handleCall}
+                />
+                {/* <div className={styles.card}>
                     <p>{group}</p>
                     <div className={styles.info}>
                         <h1>{name}</h1>
@@ -23,7 +33,8 @@ export default function IdolModal({ imgUrl, group, name, count, onClose }) {
                             <p>♥️</p>
                         </div>
                     </div>
-                </div>
+                </div> */}
+
             </div>
         </div>
     )
---
+++ src/components/Top5IdolFrame.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Top5Frame() {
+    return (
+        <div>
+            아이돌
+        </div>
+    )
+}
+
+export default Top5Frame;
